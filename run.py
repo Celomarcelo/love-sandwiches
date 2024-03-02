1c6833723ee0656cf005823c2ec54c2c19976823
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-03-02 13:25:35 -0300

    updated run.py
    function check if data is correct with right caracters

--- run.py
+++ run.py
@@ -16,14 +16,17 @@ def get_sales_data():
     """
     get the saled data from user
     """
-    print('Please enter the sales data from the last market')
-    print('Data should be six numbers, separated by comma')
-    print('ex: 00, 11, 22, 33, 44, 55\n')
-    data_str = input('Enter your data here:')
-    print(f'The data provided is {data_str}')
-    sales_data = data_str.split(',')
-    print(sales_data)
-    validate_data(sales_data)
+    while True:
+        print('Please enter the sales data from the last market')
+        print('Please enter the sales data from the last market')
+        print('Data should be six numbers, separated by comma')
+        print('ex: 00, 11, 22, 33, 44, 55\n')
+        data_str = input('Enter your data here:')
+        print(f'The data provided is {data_str}')
+        sales_data = data_str.split(',')
+        if validate_data(sales_data):
+            print('Data is valid!')
+            break
 
 def validate_data(values):
     """
@@ -31,15 +34,19 @@ def validate_data(values):
 
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
 
 
 
 
 
-get_sales_data()
+data = get_sales_data()
